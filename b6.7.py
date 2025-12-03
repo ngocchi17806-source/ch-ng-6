33c6036a731ca034ecf78497a5b5c81b9b7cd3e7
ngocchi17806-source <ngocchi17806@gmail.com>
2025-12-03 13:02:08 +0700

    Add files via upload

---
+++ b6.7.py
@@ -0,0 +1,16 @@
+print('Nguyen Ngoc Chi MSSV 245752021610164')
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * (self.radius ** 2)
+
+    def circumference(self):
+        return 2 * 3.14 * self.radius
+
+
+# Test
+c = Circle(5)
+print("Diện tích:", c.area())
+print("Chu vi:", c.circumference())
